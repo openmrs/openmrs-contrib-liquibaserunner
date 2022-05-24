95218c01eec88e4143581c797b49e41fb559c79d
rkorytkowski <rafal.korytkowski@gmail.com>
2022-05-24 12:29:08 +0200

    Fixing bash script

--- upgrade_mvp.sh
+++ upgrade_mvp.sh
@@ -3,7 +3,7 @@
 mysql -e "CREATE DATABASE IF NOT EXISTS liquibaserunner CHARACTER SET utf8 COLLATE utf8_general_ci;" -uroot
 mysql -e "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));" -uroot
 
-if [ "$TWO_STEP" = "true"]; then
+if [ "$TWO_STEP" = "true" ]; then
   mvn install -q $PROFILE -Dtest=UpgradeMVPTest -DfailIfNoTests=false -Dopenmrs.version="2.3.0" -Ddb.user="root" -Ddb.password="" -Dmvp.step=1 -Dmvp.file="$TRAVIS_BUILD_DIR/openmrs_concepts_1_6.zip"
   mvn install -q $PROFILE -Dtest=UpgradeMVPTest -DfailIfNoTests=false -Dopenmrs.version="$OPENMRS_VERSION" -Ddb.user="root" -Ddb.password="" -Dmvp.step=2 -Dmvp.file="$TRAVIS_BUILD_DIR/openmrs_concepts_1_6.zip"
 else

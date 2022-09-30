1eaad5595017fd8bc5538662fc6be8865203cc79
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2022-09-30 12:23:55 +0200

    Adding db host

--- upgrade_mvp.sh
+++ upgrade_mvp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-mysql -e "CREATE DATABASE IF NOT EXISTS liquibaserunner CHARACTER SET utf8 COLLATE utf8_general_ci;" -uroot -proot
-mysql -e "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));" -uroot -proot
+mysql -e "CREATE DATABASE IF NOT EXISTS liquibaserunner CHARACTER SET utf8 COLLATE utf8_general_ci;" -h 127.0.0.1 -uroot -proot
+mysql -e "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));" -h 127.0.0.1 -uroot -proot
 
 if [ "$TWO_STEP" = "true" ]; then
   mvn install -q $PROFILE -Dtest=UpgradeMVPTest -DfailIfNoTests=false -Dopenmrs.version="2.3.0" -Ddb.user="root" -Ddb.password="root" -Dmvp.step=1 -Dmvp.file="$TRAVIS_BUILD_DIR/openmrs_concepts_1_6.zip"
@@ -11,5 +11,5 @@ else
   mvn install -e -q $PROFILE -Dtest=UpgradeMVPTest -DfailIfNoTests=false -Dopenmrs.version="$OPENMRS_VERSION" -Ddb.user="root" -Ddb.password="root" -Dmvp.file="$TRAVIS_BUILD_DIR/openmrs_concepts_1_6.zip"
 fi
 
-mysqldump -uroot -proot liquibaserunner > "openmrs_concepts_$FILE_SUFFIX.sql"
+mysqldump -h 127.0.0.1 -uroot -proot liquibaserunner > "openmrs_concepts_$FILE_SUFFIX.sql"
 zip "openmrs_concepts_$FILE_SUFFIX.zip" "openmrs_concepts_$FILE_SUFFIX.sql"

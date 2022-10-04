74fa67b7095d6bbc2fa0020f5d98f3c5eaf66764
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2022-10-04 10:48:06 +0200

    Update upgrade_mvp.sh

--- upgrade_mvp.sh
+++ upgrade_mvp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-
-mysql -e "DROP DATABASE IF EXISTS liquibaserunner; CREATE DATABASE liquibaserunner CHARACTER SET utf8 COLLATE utf8_general_ci; SET GLOBAL max_allowed_packet=100000000;" -h 127.0.0.1 -uroot -proot
+mysql -e "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY','')); SET GLOBAL max_allowed_packet=100000000;" -h 127.0.0.1 -uroot -proot
+mysql -e "DROP DATABASE IF EXISTS liquibaserunner; CREATE DATABASE liquibaserunner CHARACTER SET utf8 COLLATE utf8_general_ci;" -h 127.0.0.1 -uroot -proot
 
 if [ "$TWO_STEP" = "true" ]; then
   mvn install -e -q $PROFILE -Dtest=UpgradeMVPTest -DfailIfNoTests=false -Dopenmrs.version="2.3.0" -Ddb.user="root" -Ddb.password="root" -Dmvp.step=1 -Dmvp.file="${GITHUB_WORKSPACE}/openmrs_concepts_1_6.zip"
